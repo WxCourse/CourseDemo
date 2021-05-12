b0cd3e5184dde4f739549e60076a40797dd4f28c
zhuyan-1 <2378976858@qq.com>
2021-05-12 22:58:21 +0800

    modify

--- wechat_Demo/pages/index/index.js
+++ wechat_Demo/pages/index/index.js
@@ -1,48 +1,146 @@
-// index.js
-// 获取应用实例
-const app = getApp()
-
+// pages/core/login/login.js
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    phone:'',//手机号
+    password:''//用户密码
   },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
   },
-  onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
-  },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
-  },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
+//表单项内容发生改变的回调
+  handleInput(event){
+    let type = event.currentTarget.dataset.type;
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      [type]: event.detail.value
     })
+  },
+
+   // 登录的回调
+   async login(){
+    // 1. 收集表单项数据
+    let {phone, password} = this.data;
+    // 2. 前端验证
+    /*
+    * 手机号验证：
+    *   1. 内容为空
+    *   2. 手机号格式不正确
+    *   3. 手机号格式正确，验证通过
+    * */
+    
+    if(!phone){
+      // 提示用户
+      wx.showToast({
+        title: '手机号不能为空',
+        icon: 'none'
+      })
+      return;
+    }
+    // 定义正则表达式
+    let phoneReg = /^1(3|4|5|6|7|8|9)\d{9}$/;
+    if(!phoneReg.test(phone)){
+      wx.showToast({
+        title: '手机号格式错误',
+        icon: 'none'
+      })
+      return;
+    }
+
+    if(!password){
+      wx.showToast({
+        title: '密码不能为空',
+        icon: 'none'
+      })
+      return;
+    }
+    // 后端验证
+    // let result = await request('/login/cellphone', {phone, password, isLogin: true})
+    // if(result.code === 200){ // 登录成功
+    //   wx.showToast({
+    //     title: '登录成功'
+    //   })
+    //   // 将用户的信息存储至本地
+    //   wx.setStorageSync('userInfo', JSON.stringify(result.profile))
+    //   // 跳转至个人中心personal页面
+    //   wx.reLaunch({
+    //     url: '/pages/personal/personal'
+    //   })
+    // }else if(result.code === 400){
+    //   wx.showToast({
+    //     title: '手机号错误',
+    //     icon: 'none'
+    //   })
+    // }else if(result.code === 502){
+    //   wx.showToast({
+    //     title: '密码错误',
+    //     icon: 'none'
+    //   })
+    // }else {
+    //   wx.showToast({
+    //     title: '登录失败，请重新登录',
+    //     icon: 'none'
+    //   })
+    // }
+  },
+
+  go:function(){
+    wx.navigateTo({
+      url: '/pages/core/main/main',
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
 })
