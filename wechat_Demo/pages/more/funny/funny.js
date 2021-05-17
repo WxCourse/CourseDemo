ec99d61f58aa7b3d18d037d30ecc3c1fcc39c229
zhuyan-1 <2378976858@qq.com>
2021-05-17 23:21:03 +0800

    Changes to be committed: 	modified:   wechat_Demo/app.json 	modified:   wechat_Demo/pages/core/main/main.wxml 	modified:   wechat_Demo/pages/core/main/main.wxss 	modified:   wechat_Demo/pages/indexNav/calendar/calendar.json 	modified:   wechat_Demo/pages/indexNav/student/student.wxml 	modified:   wechat_Demo/pages/indexNav/student/student.wxss 	modified:   wechat_Demo/pages/more/compusNews/compusNews.js 	modified:   wechat_Demo/pages/more/compusNews/compusNews.json 	modified:   wechat_Demo/pages/more/compusNews/compusNews.wxml 	modified:   wechat_Demo/pages/more/compusNews/compusNews.wxss 	modified:   wechat_Demo/pages/more/funny/funny.js 	modified:   wechat_Demo/pages/more/funny/funny.json 	modified:   wechat_Demo/pages/more/funny/funny.wxml 	modified:   wechat_Demo/pages/more/funny/funny.wxss 	modified:   wechat_Demo/project.config.json

--- wechat_Demo/pages/more/funny/funny.js
+++ wechat_Demo/pages/more/funny/funny.js
@@ -1,13 +1,50 @@
-// pages/more/funny/funny.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    NList:[
+      {
+        NewsTitle:"青春有你，唯有一起奋斗！",
+        time:"发布时间：2021/5/5",
+      },
+      { 
+        NewsTitle:"不会有人只在寝室睡觉吧？",
+        time:"发布时间：2021/4/29",
+      },
+      {
+        NewsTitle:"和全世界道了晚安，却一个人偷偷失眠",
+        time:"发布时间：2021/4/24",
+      },
+      {
+        NewsTitle:"科研“视界”，平凡学子",
+        time:"发布时间：2021/4/1",
+      },
+      {
+        NewsTitle:"缓冲完毕 即刻腾飞",
+        time:"发布时间：2021/3/29",
+      },
+      {
+        NewsTitle:"养生这件事，我琢磨好久了",
+        time:"发布时间：2021/3/26",
+      },
+      {
+        NewsTitle:"晴天 | 暖阳奔向你而来，一起晒太阳吧！",
+        time:"发布时间：2021/3/24",
+      },
+    ]
+  },
+  onSearch:function(e){
+  },
+  onCancel:function(){
 
   },
-
+  jump:function(){
+    wx.navigateTo({
+      url: '/pages/more/funny/funnyShow',
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
