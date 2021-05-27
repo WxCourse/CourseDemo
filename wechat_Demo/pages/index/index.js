f04413356a0e95733bfdb58170eacff834175da0
zhuyan-1 <2378976858@qq.com>
2021-05-27 16:33:35 +0800

    Changes to be committed: 	modified:   "document/\344\273\213\347\273\215\346\226\207\346\241\243.docx" 	modified:   wechat_Demo/app.json 	modified:   wechat_Demo/images/iconfont/iconfont.wxss 	new file:   wechat_Demo/images/nav/pic/weixin.jpg 	deleted:    wechat_Demo/page/core/user.mine.js 	deleted:    wechat_Demo/page/core/user.mine.json 	deleted:    wechat_Demo/page/core/user.mine.wxml 	deleted:    wechat_Demo/page/core/user.mine.wxss 	deleted:    wechat_Demo/page/core/user/mine.js 	deleted:    wechat_Demo/page/core/user/mine.json 	deleted:    wechat_Demo/page/core/user/mine.wxml 	deleted:    wechat_Demo/page/core/user/mine.wxss 	modified:   wechat_Demo/pages/index/index.js 	modified:   wechat_Demo/pages/index/index.wxml 	modified:   wechat_Demo/pages/index/index.wxss 	new file:   wechat_Demo/pages/index/weiXinLogin.js 	new file:   wechat_Demo/pages/index/weiXinLogin.json 	new file:   wechat_Demo/pages/index/weiXinLogin.wxml 	new file:   wechat_Demo/pages/index/weiXinLogin.wxss 	modified:   wechat_Demo/project.config.json

--- wechat_Demo/pages/index/index.js
+++ wechat_Demo/pages/index/index.js
@@ -6,8 +6,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    username:null,//用户名
-    password:null, //用户密码
+    phone: '', // 手机号
+    password: '' // 用户密码
   },
 
   /**
@@ -17,19 +17,61 @@ Page({
 
   },
 //表单项内容发生改变的回调
-loginBtnClick:function(){
-  app.appData.userinfo = {username:this.data.username,password:this.data.password}
-  wx.redirectTo({
-    url: '/pages/core/main/main',
+handleInput(event){
+  // let type = event.currentTarget.id;// id传值 取值： phone || password
+  let type = event.currentTarget.dataset.type; // data-key=value
+  // console.log(event);
+  this.setData({
+    [type]: event.detail.value
   })
 },
-  usernameInput:function(event){
-    this.setData({username:event.detail.value})
-  },
-  passwordInput:function(event){
-    this.setData({password:event.detail.value})
-  },
+ // 登录的回调
+login(){
+  // 1. 收集表单项数据
+  let {phone, password} = this.data;
+  // 2. 前端验证
+  /*
+  * 手机号验证：
+  *   1. 内容为空
+  *   2. 手机号格式不正确
+  *   3. 手机号格式正确，验证通过
+  * */
   
+  if(!phone){
+    // 提示用户
+    wx.showToast({
+      title: '手机号不能为空',
+      icon: 'none'
+    })
+    return;
+  }
+  // 定义正则表达式
+  let phoneReg = /^1(3|4|5|6|7|8|9)\d{9}$/;
+  if(!phoneReg.test(phone)){
+    wx.showToast({
+      title: '手机号格式错误',
+      icon: 'none'
+    })
+    return;
+  }
+  
+  if(!password){
+    wx.showToast({
+      title: '密码不能为空',
+      icon: 'none'
+    })
+    return;
+  }
+  wx.showToast({
+    title: '登录成功',
+  })
+},
+
+goLogin:function(){
+  wx.navigateTo({
+    url:'/pages/index/weiXinLogin',
+  })
+},
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
